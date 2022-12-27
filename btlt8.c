353fe4bd405b5edbd04edeb26ada574aeefdb4c4
NgoNhatWin <117475856+ngonhatwin@users.noreply.github.com>
2022-12-27 09:33:28 +0700

    Add files via upload

---
+++ btlt8.c
@@ -0,0 +1,13 @@
+// In t?t c? các s? nguyên duong l? nh? hon 100.
+#include<stdio.h>
+
+int main() {
+    int i;
+    
+    for ( i = 1; i < 100; i+=2) {
+    	printf ("so nguyen dl: %d\n", i);
+    	
+	} 
+		
+    
+}
