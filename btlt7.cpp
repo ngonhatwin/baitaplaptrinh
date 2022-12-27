5a9d5e0c57adb18154fe7fe5ce0a4075b3d59202
NgoNhatWin <117475856+ngonhatwin@users.noreply.github.com>
2022-12-27 08:40:13 +0700

    Add files via upload

---
+++ btlt7.cpp
@@ -0,0 +1,11 @@
+// Hi?n th? các kí t? in hoa t? A d?n Z.
+#include<stdio.h>
+
+int main() {
+    char kytu;
+    
+    for ( kytu = 65; kytu <= 90 ; kytu ++) {
+    	printf("%c\n", kytu);
+	}
+    
+}
